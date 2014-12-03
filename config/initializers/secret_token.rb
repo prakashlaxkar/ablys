24696e23a28420b0667e3b1ab6c14059ee3c1cfa
Prakash Laxkar <prakashlaxkar@gmail.com>
2014-12-03 23:09:32 +0530

    ablys added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ablys::Application.config.secret_key_base = '3d054663b50be5c9c5693d880a1a69adbb537eff0182eed427c33bb61f35d6c43bacfcd0843c123732b3956bea6258c03ac063c387c4429c20c27a6c17f0c6bf'
