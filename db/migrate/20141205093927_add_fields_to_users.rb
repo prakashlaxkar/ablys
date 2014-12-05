071ebca1219672dedb104bba613a0f2c2c9a2a49
Prakash <sodel@sodel-GA-78LMT-S2.(none)>
2014-12-05 16:10:06 +0530

    fields added to users

---
+++ db/migrate/20141205093927_add_fields_to_users.rb
@@ -0,0 +1,15 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :dob, :string
+    add_column :users, :f_name, :string
+    add_column :users, :gender, :string
+    add_column :users, :email, :string
+    add_column :users, :address, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :country, :string
+    add_column :users, :pin_code, :string
+    add_column :users, :phone, :string
+    add_column :users, :is_matrimony, :boolean, :default => false
+  end
+end
