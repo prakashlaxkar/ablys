d251e46a70d23a1514091086b7b0b6a7095e3579
Prakash Laxkar <prakashlaxkar@gmail.com>
2015-02-15 23:41:37 +0530

    UI changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,15 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
+
+  def self.city
+    ['Ajmer', 'Jaipur', 'Kota', 'Tonk', 'Udaipur', 'Bhilwara',  'Bundi']
+  end
+
+  def self.state
+    state = ['Rajashthan', 'Punjab', 'Delhi', 'Maharastra', 'Uttar Predesh', 'Bhihar', 'Hariyana', 'Karnataka']
+  end
+  
   def self.find_for_oauth(oauth_raw_data, oauth_user_data, signed_in_resource=nil )
     return User.where("(provider = '#{oauth_raw_data.provider}' AND uid = '#{oauth_raw_data.uid}') OR email='#{oauth_user_data.email}'").first || User.create!(name:oauth_user_data.name,
                             provider:oauth_raw_data.provider,
