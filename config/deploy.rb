74fabaa4cb8260591b81b5721b784015a4794ea5
Prakash Laxkar <prakashlaxkar@gmail.com>
2015-03-16 00:49:03 +0530

    deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@ require 'bundler/capistrano'
 
 set :application, 'ablys'
 set :repository, 'git@github.com:prakashlaxkar/ablys.git'
-# set :deploy_to,  "apps/ablys"
 set :applicationdir,  "/home/ubuntu/apps/ablys"
 set :user, "ubuntu"
 
@@ -13,18 +12,13 @@ set :keep_releases, 2
 set :rails_env, "production"
 set :precompile_only_if_changed, true
 
-
-# deploy config
 set :deploy_to, "/home/ubuntu/apps/ablys"
 set :deploy_via, :export
 
-# additional settings
 default_run_options[:pty] = true  # Forgo errors when deploying from windows
 default_run_options[:shell] = '/bin/bash --login'
 
-# ssh_options[:keys] = ["/home/animesh/.ssh/animesh.pem"]
-
-ssh_options[:keys] = %w(/Path/To/id_rsa)            # If you are using ssh_keys
+ssh_options[:keys] = %w(/Path/To/id_rsa)
 
 after "deploy:update_code", "deploy:copy_configs"
 
@@ -36,7 +30,6 @@ task :prod do
   set :scm_verbose, true
   server "52.10.40.52", :app, :web, :db, :primary => true
   set :deploy_env, "prod"
-  # deploy config
 
   "deploy"
 
@@ -44,31 +37,13 @@ end
 
 namespace :deploy do
 
-  task :copy_configs, :roles => :app do
-    # run "cp #{release_path}/../../shared/database.yml #{release_path}/config/database.yml"
-    # run "cp #{release_path}/config/initializers/global.rb.#{deploy_env} #{release_path}/config/initializers/global.rb"
-    # run "cp #{release_path}/config/environment.rb.#{deploy_env} #{release_path}/config/environment.rb"
-    # run "cp #{release_path}/config/sphinx.yml.#{deploy_env} #{release_path}/config/sphinx.yml"
-    # run "cp #{release_path}/public/robots.txt.#{deploy_env} #{release_path}/public/robots.txt"
-  end
-
   task :migrate, :roles => :app do
-    run "cd #{release_path} && bundle exec rake db:migrate RAILS_ENV=production  && bundle exec rake assets:precompile RAILS_ENV=production"
-  end
-
-  task :link_shared_directories do
-    run "ln -s #{shared_path}/uploads #{release_path}/uploads"
+    run "cd #{release_path} && bundle exec rake db:migrate RAILS_ENV=production"
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
 
-    run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
-    # if deploy_env=="qa"
-    #   run "#{try_sudo} mkdir -p #{release_path}/tmp/cache"
-    #   run "#{try_sudo} chown -R nobody:nobody #{release_path}/tmp/cache"
-    #   run "#{try_sudo} mkdir -p #{release_path}/tmp/views"
-    #   run "#{try_sudo} chown -R nobody:nobody #{release_path}/tmp/views"
-    # end
+    run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"    
     if deploy_env == 'prod'
       tag_name = Time.now.strftime("deploy_%Y_%m_%d_%H_%M")
 
@@ -81,11 +56,6 @@ namespace :deploy do
     end
   end
 
-
-  #task :pipeline_precompile do
-  #  run "cd #{release_path}; /usr/local/rvm/gems/ruby-1.9.2-p290/bin/bundle install"
-  #  run "cd #{release_path}; /usr/local/rvm/gems/ruby-1.9.2-p290/bin/bundle exec rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile"
-  #end
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       logger.info "Skipping asset pre-compilation because there were no asset changes"
@@ -93,5 +63,5 @@ namespace :deploy do
   end
 end
 
+
 after "deploy:update", "deploy:migrate", "deploy:cleanup"
-after "deploy:update_code", "deploy:link_shared_directories"
